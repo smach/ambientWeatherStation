8e2092513fc6cacd3c897a02fa49ae70ec01e631
smach <smachlis@gmail.com>
2019-08-02 20:47:17 -0400

    init commit

---
+++ R/ambient_get_wx.R
@@ -0,0 +1,41 @@
+#' Get data from an Ambient weather station
+#'
+#' @param n numeric Integer for how many observations you want returned. Defaults to 1.
+#' @param the_date Date or character string in yyyy-mm-dd format. Ending date (UTC) for requested observation(s).
+#' @param device_mac_address character string for device MAC address.
+#' @param app_key character string with your Ambient application key.
+#' @param api_key character string with your Ambient API key.
+#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' #'\dontrun{
+#' ambient_get_wx(6) #for most recent 6 observations
+#' }
+ambient_get_wx <- function(n=1, the_date = Sys.Date(), device_mac_address = Sys.getenv("AMBIENT_MAC_ADDRESS"), app_key = Sys.getenv("AMBIENT_APP_KEY"), api_key = Sys.getenv("AMBIENT_API_KEY")){
+  if(nchar(device_mac_address) < 2) {
+    warning("You need your weather station's MAC address to use this package. See the Setup vignette for more details.")
+  } else if(nchar(app_key) < 2){
+    warning("You need an Ambient app key to use this package. See the Setup vignette for more details.")
+  } else if (nchar(api_key) < 2) {
+    warning("You need an Ambient API key to use this package. See the Setup vignette for more details.")
+  }  else {
+    url <- paste0("https://api.ambientweather.net/v1/devices/", device_mac_address, "?apiKey=", api_key, "&applicationKey=", app_key, "&limit=",n, "&endDate=", the_date)
+
+    resp <- httr::GET(url)
+    if (httr::http_type(resp) != "application/json") {
+      stop("API did not return json", call. = FALSE)
+    }
+
+    parsed <- jsonlite::fromJSON(httr::content(resp, as = "text"), simplifyVector = TRUE)
+    parsed$datelocaltime <- lubridate::as_datetime(parsed$dateutc / 1000, tz = Sys.timezone())
+    parsed$dateutc <- lubridate::as_datetime(parsed$dateutc / 1000, tz = "GMT")
+    lastcol <- ncol(parsed)
+    parsed <- parsed[,c(1,lastcol, 2:lastcol - 1)]
+    return(parsed)
+  }
+
+}
+
+
